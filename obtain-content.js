0620a3a8ae594ff1d1e46c7a338fd35822f0ac53
Ondřej Dušek <ondrej.dusek@pasante.cz>
2015-09-14 12:50:54 +0200

    opraveno načítání stránky s redirectem

--- obtain-content.js
+++ obtain-content.js
@@ -2,7 +2,9 @@ var system = require('system'),
 	url = system.args[1],
 	page = require('webpage').create(),
 	log = require('./logger').create(),
-	resourceManager = require('./resource-manager').create(page);
+	resourceManager = require('./resource-manager').create(page),
+	loadWait = 50,
+	loadHandle;
 
 setTimeout(function () {
 	log.error('script timeout');
@@ -19,23 +21,36 @@ page.viewportSize = {
 	width: 1024,
 	height: 768
 };
+
 page.onError = function (msg) {
 	log.error(msg);
 };
+
 page.onConsoleMessage = function (msg) {
 	log.warn('[console]', msg);
 };
 
+page.onLoadStarted = function() {
+	clearTimeout(loadHandle);
+};
+
+page.onLoadFinished = function(status) {
+	// redirect may start another loading
+	loadHandle = setTimeout(function () {
+		if (status !== 'success') {
+			log.error('page loading failed');
+			log.error(page.url, url);
+			phantom.exit(1);
+			return;
+		}
+		log.info('waiting for resources...');
+		resourceManager.load(function () {
+			log.info('loading finished');
+			system.stdout.write(page.content);
+			phantom.exit();
+		});
+	}, loadWait);
+};
+
 log.info('requesting', url);
-page.open(url, function (status) {
-	if (status !== 'success') {
-		log.error('page loading failed');
-		phantom.exit(1);
-	}
-	log.info('waiting for resources...');
-	resourceManager.load(function () {
-		log.info('loading finished');
-		system.stdout.write(page.content);
-		phantom.exit();
-	});
-});
+page.open(url);
